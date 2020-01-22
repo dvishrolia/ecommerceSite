bdc18c2e7e91414156250e6bbbcec8918f9c2df3
dvishrolia <dharmeshvishrolia@gmail.com>
2020-01-22 14:59:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 <div class="row">
 <div class="col">
     <img src="images/1.jpg" width="100%" />
+    <hr>
 </div>
+</div>
+
+<div class="row">
+    <div class="col primary">
+        
+    </div>
 </div>
 
 <?php include "footer.php" ?>
